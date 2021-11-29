16d485c51aff3b308e9d4e96d2d8e48151884bec
Krav1tZz <90614426+Krav1tZz@users.noreply.github.com>
2021-11-29 09:41:49 +0200

    Add files via upload

---
+++ MadTeext.py
@@ -0,0 +1,20 @@
+from array import *
+madText1 = ["", "", "", "", ""]
+madText2 = ["", "", "", "", ""]
+madText3 = ["", "", "", "", ""]
+madText4 = ["", "", "", "", ""]
+madText5 = ["", "", "", "", ""]
+
+def pirmaiSpeletajs():
+    madText1[0] = input("KADS?")
+    madText5[1] = input("KAS?")
+    madText4[2] = input("KUR?")
+    madText3[3] = input("ko darija")
+    madText2
+
+
+
+
+if __name__ == '__main__':
+  print("MadText")
+  speletajs1 = input("speletajs1vards")
