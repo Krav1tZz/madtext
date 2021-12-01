9239e9ae2b31d0e3f45673de6f0e984eea5d6ccc
Krav1tZz <90614426+Krav1tZz@users.noreply.github.com>
2021-12-01 14:42:56 +0200

    Update MadTeext.py

--- MadTeext.py
+++ MadTeext.py
@@ -1,4 +1,3 @@
-from array import *
 madText1 = ["", "", "", "", ""]
 madText2 = ["", "", "", "", ""]
 madText3 = ["", "", "", "", ""]
@@ -6,15 +5,75 @@ madText4 = ["", "", "", "", ""]
 madText5 = ["", "", "", "", ""]
 
 def pirmaiSpeletajs():
-    madText1[0] = input("KADS?")
+    speletajs1 = input("Ka sauc 1 speletajs?")
+    speletajs1 = speletajs1.title()
+    print(speletajs1 + ", atbildi uz 5 jautājumiem")
+
+    madText1[0] = input("KĀDS")
     madText5[1] = input("KAS?")
     madText4[2] = input("KUR?")
-    madText3[3] = input("ko darija")
-    madText2
+    madText3[3] = input("KO DARĪJA?")
+    madText2[4] = input("KAS SANĀCA?")
 
 
+def otraiSpeletajs():
+    speletajs2 = input("Ka sauc 2 speletajs?")
+    speletajs2 = speletajs2.title()
+    print(speletajs2 + ", atbildi uz 5 jautājumiem")
 
+    madText2[0] = input("KĀDS")
+    madText1[1] = input("KAS?")
+    madText5[2] = input("KUR?")
+    madText4[3] = input("KO DARĪJA?")
+    madText3[4] = input("KAS SANĀCA?")
+
+
+def tresaiSpeletajs():
+    speletajs3 = input("Ka sauc 3 speletajs?")
+    speletajs3 = speletajs3.title()
+    print(speletajs3 + ", atbildi uz 5 jautājumiem")
+
+    madText3[0] = input("KĀDS")
+    madText2[1] = input("KAS?")
+    madText1[2] = input("KUR?")
+    madText5[3] = input("KO DARĪJA?")
+    madText4[4] = input("KAS SANĀCA?")
+
+
+def ceturtaiSpeletajs():
+    speletajs4 = input("Ka sauc 4 speletajs?")
+    speletajs4 = speletajs4.title()
+    print(speletajs4 + ", atbildi uz 5 jautājumiem")
+
+    madText4[0] = input("KĀDS")
+    madText3[1] = input("KAS?")
+    madText2[2] = input("KUR?")
+    madText1[3] = input("KO DARĪJA?")
+    madText5[4] = input("KAS SANĀCA?")
+
+
+def piektaiSpeletajs():
+    speletajs5 = input("Ka sauc 5 speletajs?")
+    speletajs5 = speletajs5.title()
+    print(speletajs5 + ", atbildi uz 5 jautājumiem")
+
+    madText5[0] = input("KĀDS")
+    madText4[1] = input("KAS?")
+    madText3[2] = input("KUR?")
+    madText2[3] = input("KO DARĪJA?")
+    madText1[4] = input("KAS SANĀCA?")
 
 if __name__ == '__main__':
-  print("MadText")
-  speletajs1 = input("speletajs1vards")
+  print("MadText. Katrs spēlētājs atbild uz jautājumu,"
+        " aizloka lapiņu un padot tālāk nākamajam spēlētājam līdz ir atbildēts uz visiem jautājumiem."
+        " Spēles beigās tiek nolasīti teikumi, kas izveidojušies spēles rezultātā.")
+  pirmaiSpeletajs()
+  otraiSpeletajs()
+  tresaiSpeletajs()
+  ceturtaiSpeletajs()
+  piektaiSpeletajs()
+  print(madText1)
+  print(madText2)
+  print(madText3)
+  print(madText4)
+  print(madText5)
